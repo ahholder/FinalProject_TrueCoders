4e10b70a806a3c90a94730fb92a33f5e16e4b451
Alexander Holder <drowish95z@gmail.com>
2023-04-03 11:38:02 -0500

    Images Added

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -58,5 +58,19 @@ namespace Testing.Models
             if (job == 9) return "Warrior";
             return "Other";
         }
+        public string VisualAid(int id)
+        {
+            if (id == 0) return "https://1001freedownloads.s3.amazonaws.com/vector/thumb/98360/opensourcebear_guitar.png"; //Bard
+            if (id == 1) return "https://static.vecteezy.com/system/resources/previews/001/194/209/non_2x/cross-gothic-png.png"; //Cleric
+            if (id == 2) return "https://www.nicepng.com/png/full/117-1176628_simple-black-leaf-clip-art-at-clker-black.png"; //Druid
+            if (id == 3) return "https://www.clipartqueen.com/image-files/open-book-silhouette.png"; //Mage
+            if (id == 4) return "https://www.pngplay.com/wp-content/uploads/15/Karate-Black-Belt-Transparent-File.png"; //Monk
+            if (id == 5) return "https://freepngimg.com/save/24094-shield-clip-art-black-and-white-transparent/504x600"; //Paladin
+            if (id == 6) return "https://www.freeiconspng.com/thumbs/bow-and-arrow-png/bow-and-arrow-png-picture-3.png"; //Ranger
+            if (id == 7) return "https://pngimg.com/d/dagger_PNG48.png"; //Rogue
+            if (id == 8) return "https://i.kisscc0.com/20180705/baw/kisscc0-vampire-the-masquerade-vampire-the-eternal-strug-dracoconic-ouroboros-symbol-5b3dfb3458aa32.3012231015307886603632.png"; //Warlock
+            if (id == 9) return "https://www.pngmart.com/files/1/Cross-Sword-PNG-Clipart.png"; //Warrior
+            return "https://www.freeiconspng.com/thumbs/gear-icon/gear-icon-13.png"; //Other
+        }
     }
 }
